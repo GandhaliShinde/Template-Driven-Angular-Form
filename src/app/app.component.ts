628b6d33631937c1a0f70c155e22b2ed0e1d5a28
Gandhali <gandhalishinde27@gmail.com>
2022-09-23 17:49:39 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'working-form';
+  
+nameError:string = '';
+emError:string = '';
+pwError:string = '';
+mobError:string = '';
+addrError:string = '';
+regexpEmail:RegExp = new RegExp("a-zA-Z0-9");
+
+  sendData(data:NgForm): void{
+      console.log("test");
+      console.log("data:" + data.value);
+      console.log("user:" + data.controls['email'].value)
+
+
+      if( data.controls['fname'].value.length == 0 ||  data.controls['lname'].value )
+          this.nameError = 'Name required'
+
+      if( data.controls['address'].value.length == 0)
+          this.addrError = 'Address required';
+
+      if( data.controls['mobile'].value.length == 0)
+          this.mobError = 'Phone required';
+
+      if( data.controls['email'].value.length == 0)
+          this.emError = 'Email required';
+
+      if( data.controls['password'].value.length == 0)
+          this.pwError = 'Password required';
+
+      if( data.controls['rpassword'].value.length == 0)
+          this.pwError = 'Confirmation required';
+
+
+      if( data.controls['fname'].value.length > 15 ||  data.controls['lname'].value)
+          this.nameError = 'Name cannot be greater that 15 characters';
+
+      if(! data.controls['email'].value.includes('@'))
+      this.emError = 'Email should be in proper format';
+
+      if( data.controls['password'].value !=  data.controls['rpassword'].value)
+      this.pwError = 'Password mismatch'
+
+      if( data.controls['mobile'].value.length < 10 || data.controls['mobile'].value.length > 10 || data.controls['mobile'].value.includes('a-z')) 
+          this.mobError = 'Phone number not in correct format';
+ 
+         if(!this.regexpEmail.test(data.controls['password'].value)){
+          this.pwError = 'Password should contain only numbers and characters';
+         } 
+    }
+  
+
+
 }
